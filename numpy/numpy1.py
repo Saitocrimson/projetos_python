0db0de6665bb2fe886abf9108be26afd3222b4cf
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2023-10-25 19:04:53 -0300

    Add files via upload

---
+++ numpy/numpy1.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def menu():
+    print("1-Array simples\n2-Array em Tupla\n3-Array em Matriz\n4-Função ndim e ndmin\n5-Separar Array\n6-Sair")
+    
+def escolha(n):
+    if n==1:
+        arr = np.array([1, 2, 3, 4, 5])
+        print(arr)
+        return 0
+    elif n==2:
+        arr2 = np.array((1, 2, 3, 4, 5))
+        print(arr2)
+        return 0
+    elif n==3:
+        arr3 = np.array([[1, 2, 3,4], [5, 6, 7,8]])
+        print(arr3)
+        print("Para achar algum numero e so escrever matriz[linha,coluna], ex:arr[1,0]->", arr3[1,0])
+        return 0
+    elif n==4:
+        a = np.array(42)
+        b = np.array([1, 2, 3, 4, 5])
+        c = np.array([[1, 2, 3], [4, 5, 6]])
+        d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+        print('numeros de dimensões, usa a função ndim, ex array.ndim : ')
+        print(a,"->",a.ndim,"\n")
+        print(b,"->",b.ndim,"\n")
+        print(c,"->",c.ndim,"\n")
+        print(d,"->",d.ndim,"\n")
+        print('cria o números de dimensões, usa a função ndmin no final da função np.array como parametro: ')
+        print("ex:exemplo=np.array([1, 2,3,4], ndmin=7 ")
+        arr4 = np.array([1, 2, 3, 4], ndmin=7)
+        print(arr4)
+        return 0
+    elif n==5:
+        print("Para separar uma parte do array ->array[inicia a parte que quer cortar, final da parte]")
+        arr = np.array([1, 2, 3, 4, 5])
+        print(arr)
+        print("ex:array[2:3]")
+        print(arr[2:3])
+        print("ex:array[1:2:3]")
+        print(arr[1:2:3])
+        print("final->array[-1]")
+        print(arr[-1])
+        return 0
+    elif n==6:
+        return 1
+    
+    else:
+        print("escolha algo viavel")
+        return 0
+
+        
+while True:
+    menu()
+    n=int(input("Digite a opção;"))
+    print("\n\n")
+    if escolha(n)==1:
+        break
+    print("\n\n")
+        
+   
+
+
+
+
+
