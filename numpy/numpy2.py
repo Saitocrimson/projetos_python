fe04baa41d6fde73f72b0f78e6444373fb6f7fbc
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2023-10-27 20:10:45 -0300

    Add files via upload

---
+++ numpy/numpy2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+y = arr.view()
+arr[0] = 42
+
+#modifica a original
+#x = arr.view()
+#x[0] = 31
+print(arr)
+print(x)
+print(y.base,x.base)
